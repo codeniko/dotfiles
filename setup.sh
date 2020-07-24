97423c34aae4bf2984f872287295b9739040e0f2
Nikolay Feldman <niko@niko.rocks>
2020-07-23 19:10:51 -0700

    alias and more to setup script

--- setup.sh
+++ setup.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-# Deps: npm or nvm
-# python
+# Things to manually install
+# Ulauncher https://ulauncher.io/#Download
+
+apt-get install -y telnet dnsutils curl vim git redshift
+
+curl -sL https://deb.nodesource.com/setup_14.x | bash -
+apt-get install -y nodejs
+# Verify node and npm versions
+node -v; npm -v
 
 npm install -g diff-so-fancy
